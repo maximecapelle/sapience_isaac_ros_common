0f95f15256af042c5bf67efe20fd72dee891a376
sapiencelaptop <sapience.tudelft@gmail.com>
2024-10-15 19:28:58 +0200

    lastcommitfromworkstation

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -13,19 +13,19 @@ echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
 # # Ardupilot_ws -------------------
-# cd /workspaces/ardupilot_ws/ && rosdep install -r --from-paths src -i -y --rosdistro humble && 
-# export PATH=$PATH:/workspaces/ardupilot_ws/src/Micro-XRCE-DDS-Gen/scripts
-# export PATH=$PATH:/workspaces/ardupilot_ws/src/ardupilot/Tools/autotest
-# # colcon build
-# . install/setup.bash
-# cd /workspaces/sapience_nvidia_devel
+cd /workspaces/ardupilot_ws/ && rosdep install -r --from-paths src -i -y --rosdistro humble && 
+export PATH=$PATH:/workspaces/ardupilot_ws/src/Micro-XRCE-DDS-Gen/scripts
+export PATH=$PATH:/workspaces/ardupilot_ws/src/ardupilot/Tools/autotest
+# colcon build
+. install/setup.bash
+cd /workspaces/sapience_nvidia_devel
 # # --------------------------------
 
 # # Sapience Devel ws --------------
-# sudo apt-get update
-# rosdep update
-# export GZ_VERSION=garden
-# rosdep install -r --from-paths src -i -y --rosdistro humble
+sudo apt-get update
+rosdep update
+export GZ_VERSION=garden
+rosdep install -r --from-paths src -i -y --rosdistro humble
 # # --------------------------------
 
 # Return to starting point
@@ -35,8 +35,9 @@ source ../ardupilot_ws/install/setup.bash
 cp ./src/SAPIENCE/sim_launch3d.py .
 cp ./src/SAPIENCE/drone_launch.py .
 
-# #install geographic bullshit with root privileges
-# sudo ./src/isaac_ros_common/install_geographiclib_datasets.sh
+
+sudo apt-get install -y ros-${ROS_DISTRO}-isaac-ros-unet && sudo rm -rf /var/lib/apt/lists/*
+
 
 # Restart udev daemon
 sudo service udev restart
